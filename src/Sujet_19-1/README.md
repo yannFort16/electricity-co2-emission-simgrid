f98e3ef4d5192458d0a23b2d10524c65de79e901
yannFort16 <145565224+yannFort16@users.noreply.github.com>
2025-03-23 13:47:48 +0100

    sujet19 test file

---
+++ src/Sujet_19-1/README.md
@@ -0,0 +1,25 @@
+# Sujet_19 Project Overview
+
+The `Sujet_19` directory contains the implementation of a simulation project utilizing SimGrid's S4U API. This project is designed to simulate computational tasks while tracking CO2 emissions.
+
+## Project Structure
+
+- **s4u-emission-exec1/**: This directory contains the main simulation implementation.
+  - **src/**: Contains the source code files for the simulation.
+    - **s4u-emission-exec1.cpp**: The main implementation file that defines the simulation logic and execution.
+  - **CMakeLists.txt**: The CMake configuration file for building the project.
+  - **README.md**: Documentation specific to the `s4u-emission-exec1` project.
+
+## Purpose
+
+The purpose of this project is to demonstrate the capabilities of SimGrid's S4U API in simulating distributed applications and measuring their environmental impact through CO2 emissions.
+
+## Building and Running
+
+To build and run the simulation, navigate to the `s4u-emission-exec1` directory and follow the instructions in its README.md file. Ensure that you have the necessary dependencies installed, including the SimGrid library.
+
+## Dependencies
+
+- SimGrid library: Make sure to have the appropriate version of SimGrid installed and configured in your environment.
+
+For more detailed instructions on building and running the simulation, please refer to the `s4u-emission-exec1/README.md` file.
---
+++ src/Sujet_19-1/s4u-emission-exec1/src/s4u-emission-exec1.cpp
@@ -0,0 +1,40 @@
+// filepath: /Sujet_19/s4u-emission-exec1/src/s4u-emission-exec1.cpp
+#include "simgrid/s4u.hpp"
+#include "/home/yann/newSimGrid/simgrid/include/simgrid/plugins/emission.h"
+
+XBT_LOG_NEW_DEFAULT_CATEGORY(s4u_test, "Messages specific for this s4u example");
+namespace sg4 = simgrid::s4u;
+
+void test_execution() {
+    sg4::Host* host1 = sg4::Host::by_name("MyHost1");
+    sg4::Host* host2 = sg4::Host::by_name("MyHost2");
+
+    double start = sg4::Engine::get_clock();
+    XBT_INFO("Sleep for 10 seconds");
+    sg4::this_actor::sleep_for(10);
+    XBT_INFO("Done sleeping (duration: %.2f s); CO2 emission = %.2f g",
+             sg4::Engine::get_clock() - start, sg_host_get_emission(sg4::Host::by_name("MyHost1")));
+
+    start = sg4::Engine::get_clock();
+    double flopAmount = 100E6;
+    XBT_INFO("Run a computation of %.0E flops", flopAmount);
+    sg4::this_actor::execute(flopAmount);
+    XBT_INFO(
+      "Computation done (duration: %.2f s). Current peak speed=%.0E flop/s; CO2 Emission =%.0f g",
+      sg4::Engine::get_clock() - start, host1->get_speed(), sg_host_get_emission(host1));
+}
+
+int main (int argc, char *argv[]) {
+    sg_host_emission_plugin_init();
+    sg4::Engine e(&argc, argv);
+
+    xbt_assert(argc == 2, "Usage: %s platform_file\n\tExample: %s ../platforms/emission_platform.xml\n", argv[0], argv[0]);
+
+    e.load_platform(argv[1]);
+  
+    e.add_actor("emission test", e.host_by_name("MyHost1"), test_execution);
+    
+    XBT_INFO("End of simulation.");
+    e.run();
+    return 0;
+}
